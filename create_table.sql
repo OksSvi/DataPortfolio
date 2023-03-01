abe793c1ac994d688f26a366f04e4bc430bf26ed
OksSvi <126670559+OksSvi@users.noreply.github.com>
2023-03-01 12:58:53 +0200

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,14 @@
+create table site_monthly_visits
+	(
+		site VARCHAR(20);	
+		country	INT;
+		year INT;	
+		month INT;	
+		visits FLOAT()
+		)
+	;
+
+insert into site_monthly_visits
+values (site, country, year, month, visits)
+
+
